f045e36b8b221d009202cec1ba2d9cf38ef71c37
stniyaz <bd7vecazg@code.edu.az>
2025-01-29 15:46:17 +0400

    orderServiceCreatedWithOnionArchitectureNotCompleted

---
+++ MultiShop/src/Services/Order/Core/MultiShop.Order.Domain/Entities/Ordering.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Order.Domain.Entities;
+
+public class Ordering
+{
+    public string OrderingId { get; set; }
+    public string UserId { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime OrderDate { get; set; }
+
+    public List<OrderDetail> OrderDetails { get; set; }
+}
