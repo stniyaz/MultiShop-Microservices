c0c489986e1040686d75ebd482f1658d839fea5f
stniyaz <bd7vecazg@code.edu.az>
2025-02-07 03:25:18 +0400

    idenityServer4Installed

--- MultiShop/src/Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
+++ MultiShop/src/Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af852334b832356f38d0e32469b5b7540662397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4778854103f9bd0f2a88c0605f39a9096bffd551")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
